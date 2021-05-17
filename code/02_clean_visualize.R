436ea1d682ed3505491d09ebb284abf1a40ffd5b
Ryan Peek <rapeek@ucdavis.edu>
2021-05-17 12:05:04 -0700

    update

--- code/02_clean_visualize.R
+++ code/02_clean_visualize.R
@@ -58,27 +58,66 @@ df_ts <- df_clean %>%
   select(ID, PARAM, Date, Flow, SYM, Agency) %>%
   create.ts()
 
+
+# STATS -------------------------------------------------------------------
+
+
 # use pk.cov function to calc center of volume
 df_cov <- pk.cov(df_ts)
+
+# which is basically this:
+# tst <- df_clean %>% group_by(water_year) %>% summarize(totvol=cumsum(Flow)/sum(Flow)) %>% bind_cols(., df_clean[,c(10,3,5)])
+
+# get WYT
+wyt <- read_csv("data_raw/WYT_1906-2021.csv") %>%
+  mutate(Sac_WY_type=factor(Sac_WY_type, levels=c("W","AN","BN","D","C")))
+
+# join w data
+df_cov <- left_join(df_cov, wyt %>% select(WY, Sac_WY_type), by=c("hYear"="WY"))
+
+# look at drought severity:
+df_drought <- FlowScreen::dr.seas(df_ts)
+df_drought2 <- df_ts %>% filter(hyear>2019) %>% FlowScreen::dr.events()
+df_drought$hyear <- year(attr(df_drought$StartDay, "times"))
+
+# get mean by water year type for Center of Vol
+mean_wy_cov <- df_cov %>% group_by(Sac_WY_type) %>% summarize(mean50=mean(Q50),
+                                                              mean25=mean(Q25),
+                                                              mean75=mean(Q75))
+
+# get quantiles
+(thresh <- df_ts %>% filter(hyear==2017) %>% group_by(hyear) %>% summarize(quants = quantile(Flow, 0.2, na.rm=TRUE)))
 
 
 # VISUALIZE ---------------------------------------------------------------
 
 # center of volume plot
-plot_cov <-
-  ggplot() +
-  geom_pointrange(data=df_cov, aes(x=hYear, y=Q50, ymin=Q25, ymax=Q75, fill=Q50),
-                  pch=21, size=0.6,  alpha=0.8,
-                  color="gray20", show.legend=FALSE) +
-  scale_fill_viridis_c() +
-  scale_x_continuous(breaks = seq(1940, year(Sys.Date()),3)) +
-  theme_bw(base_size = 10) +
-  theme(axis.text.x = element_text(angle=330,
-                                   hjust = 0.4,
-                                   vjust = 0.1 )) +
-  labs(x="Water Year", y="Center of Volume 50% \n(Day of year for tot ann streamflow)")
+(plot_cov <-
+   ggplot() +
+   geom_point(data=df_cov, aes(y=hYear, x=Q50, fill=Sac_WY_type),
+              pch=21, size=3.5, alpha=0.9,
+              color="gray20", show.legend=TRUE) +
+   geom_vline(data=mean_wy_cov, aes(xintercept=mean50, color=Sac_WY_type), lty=2) +
+   scale_fill_viridis_d("Water Year\n Type") +
+   scale_color_viridis_d("Water Year\n Type") +
+   scale_y_continuous(breaks = seq(1940, year(Sys.Date()),5)) +
+   theme_bw(base_size = 10) +
+   labs(y="Water Year", x="Center of Volume\n(Day of year for 50% of tot. ann streamflow)")
+)
 
-print(plot_cov)
+ggsave(plot_cov, filename = "output/figure_center_of_volume_Q50.png",
+       dpi=200, width=10, height = 6.5)
 
-ggsave(plot_cov, filename = "output/figure_center_of_volumne_Q25-Q75.png",
+# drought metrics
+ggplot(data=df_drought) +
+  geom_linerange(aes(xmax=EndDay, xmin=StartDay, y=hyear, color=Severity)) +
+  geom_point(aes(x=StartDay, y=hyear), pch=21, size=2, fill="orange") +
+  geom_point(aes(x=EndDay, y=hyear), pch=21, size=3, fill="maroon") +
+  scale_color_viridis_c("Severity") +
+  scale_y_continuous(breaks = c(seq(1941, 2021, 4))) +
+  theme_classic() +
+  labs(title="Start and end of significant droughts",
+       subtitle="Severity of drought based on 30-day window using a flow duration curve (Beyene et al. 2014)")
+# save
+ggsave(plot_cov, filename = "output/figure_drought_periods.png",
        dpi=200, width=10, height = 6.5)
